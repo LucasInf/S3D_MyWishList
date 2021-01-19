b0ba98bfb16bdab58ea54f87e2c82b214730be42
Quentin Donadoni <quentin.donadoni@gmail.com>
2021-01-19 15:24:13 +0100

    changement final du site en fonction de la connexion en optimisant le programme

--- Programme/V6/PHP/src/vue/VueAjoutImage.php
+++ Programme/V6/PHP/src/vue/VueAjoutImage.php
@@ -35,57 +35,47 @@ FIN;
                 break;
             }
         }
-
         $url_accueil    = $this->container->router->pathFor( 'racine'                 ) ;
-        $url_choixajout_image    = $this->container->router->pathFor( 'choixajoutImage'                 ) ;
-        $url_deconnexion   = $this->container->router->pathFor( 'deconnexion'               ) ;
+        $url_listes     = $this->container->router->pathFor( 'aff_listes'             ) ;
+        $url_voslistes     = $this->container->router->pathFor( 'aff_voslistes'             ) ;
+        $url_form_liste = $this->container->router->pathFor( 'formListe'              ) ;
         $url_formlogin  = $this->container->router->pathFor( 'formlogin'              ) ;
         $url_testform   = $this->container->router->pathFor( 'testform'               ) ;
+        $url_deconnexion   = $this->container->router->pathFor( 'deconnexion'               ) ;
+
 
         if(isset($_SESSION['login'])) {
-            $html = <<<FIN
+            $ada = "<li><a href=".$url_voslistes.">Vos Listes</a></li>
+				<li><a href=".$url_form_liste.">Nouvelle Liste</a></li>
+				<li><a href=".$url_deconnexion.">Deconnexion</a></li>";
+        }else{
+            $ada = "<li><a href=".$url_formlogin.">S'inscrire</a></li>
+			<li><a href=".$url_testform.">Se connecter</a></li>";
+
+        }
+        $html = <<<FIN
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Accueil</title>
-    <link rel="stylesheet" href="../CSS/design.css" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="CSS/design.css" />
+    <title>Exemple</title>
   </head>
   <body>
 		<h1><a href="$url_accueil">Wish List</a></h1>
 		<nav>
 			<ul>
 				<li><a href="$url_accueil">Accueil</a></li>
-				<li><a href="$url_choixajout_image">Ajouter Image</a></li>
-				<li><a href="$url_deconnexion">Deconnexion</a></li>
+				<li><a href="$url_listes">Listes</a></li>
+				$ada
+				
 			</ul>
 		</nav>
     $content
   </body>
 </html>
 FIN;
-        }else {
-            $html = <<<FIN
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Accueil</title>
-    <link rel="stylesheet" href="CSS/design.css" />
-  </head>
-  <body>
-		<h1><a href="$url_accueil">Wish List</a></h1>
-		<nav>
-		    <strong>
-			    <ul>
-			        <li><a href="$url_accueil">Accueil</a></li>
-			    	<li><a href="$url_choixajout_image">Ajouter image</a></li>
-			    </ul>
-			</strong>
-		</nav>
-    $content
-  </body>
-</html>
-FIN;
-        }
+
         return $html;
     }
 }
--- Programme/V6/PHP/src/vue/VueAjoutMessage.php
+++ Programme/V6/PHP/src/vue/VueAjoutMessage.php
@@ -38,19 +38,31 @@ FIN;
             }
 
         }
-
         $url_accueil    = $this->container->router->pathFor( 'racine'                 ) ;
         $url_listes     = $this->container->router->pathFor( 'aff_listes'             ) ;
+        $url_voslistes     = $this->container->router->pathFor( 'aff_voslistes'             ) ;
         $url_form_liste = $this->container->router->pathFor( 'formListe'              ) ;
         $url_formlogin  = $this->container->router->pathFor( 'formlogin'              ) ;
         $url_testform   = $this->container->router->pathFor( 'testform'               ) ;
+        $url_deconnexion   = $this->container->router->pathFor( 'deconnexion'               ) ;
 
+
+        if(isset($_SESSION['login'])) {
+            $ada = "<li><a href=".$url_voslistes.">Vos Listes</a></li>
+				<li><a href=".$url_form_liste.">Nouvelle Liste</a></li>
+				<li><a href=".$url_deconnexion.">Deconnexion</a></li>";
+        }else{
+            $ada = "<li><a href=".$url_formlogin.">S'inscrire</a></li>
+			<li><a href=".$url_testform.">Se connecter</a></li>";
+
+        }
         $html = <<<FIN
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="CSS/design.css" />
     <title>Exemple</title>
-    <link rel="stylesheet" href="../CSS/design.css" />
   </head>
   <body>
 		<h1><a href="$url_accueil">Wish List</a></h1>
@@ -58,6 +70,8 @@ FIN;
 			<ul>
 				<li><a href="$url_accueil">Accueil</a></li>
 				<li><a href="$url_listes">Listes</a></li>
+				$ada
+				
 			</ul>
 		</nav>
     $content
